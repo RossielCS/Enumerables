af157c59dd8daeeaf17793b87213d2c4033b59eb
Rossiel Carranza <rossielcarranza@gmail.com>
2020-04-13 14:41:34 -0300

    Add my_select
    -  my_select use my_each method

--- main.rb
+++ main.rb
@@ -20,8 +20,18 @@ module Enumerable
     end
     self
   end
+
+  def my_select
+    return to_enum(:my_select) unless block_given?
+
+    result = []
+    my_each { |i| result << i if yield(i) }
+    result
+  end
 end
 
-[11, 79, 15, 43, 38, 1].my_each { |items| puts items * 2 }
+# [11, 79, 15, 43, 38, 1].my_each { |items| puts items * 2 }
 
-[27, 76, 48, 37, 24, 12, 47].my_each_with_index { |_x, y| puts y }
+# [27, 76, 48, 37, 24, 12, 47].my_each_with_index { |_x, y| puts y }
+
+[6, 9, 47, 59, 86, 11, 30].my_select { |x| (x % 3).zero? }
