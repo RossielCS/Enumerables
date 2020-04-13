3dc4887eba1db1d43289b36cd0d2dd0139e74e43
Rossiel Carranza <rossielcarranza@gmail.com>
2020-04-13 18:06:06 -0300

    Add method my_all?
    - Use method my_each
    - disable style case equality cop

--- main.rb
+++ main.rb
@@ -28,10 +28,23 @@ module Enumerable
     my_each { |i| result << i if yield(i) }
     result
   end
+
+  def my_all?
+    unless block_given?
+      my_each { |i| return false if i.nil? || i == false }
+      return true
+    end
+
+    count = 0
+    my_each { |i| count += 1 if yield(i) }
+    count == length
+  end
 end
 
 # [11, 79, 15, 43, 38, 1].my_each { |items| puts items * 2 }
 
 # [27, 76, 48, 37, 24, 12, 47].my_each_with_index { |_x, y| puts y }
 
-[6, 9, 47, 59, 86, 11, 30].my_select { |x| (x % 3).zero? }
+# [6, 9, 47, 59, 86, 11, 30].my_select { |x| (x % 3).zero? }
+
+# puts(%w[a abc hello names dialog telephone bo].my_all? { |x| x.length >= 1 })
