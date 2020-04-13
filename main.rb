5ea2af71716617f52ab38fe17aabce9fa72352e7
Rossiel Carranza <rossielcarranza@gmail.com>
2020-04-13 13:56:15 -0300

    Add my_each_with_index
    - Method identical to each_with_index
    - Use yield statement

--- main.rb
+++ main.rb
@@ -9,6 +9,19 @@ module Enumerable
     end
     self
   end
+
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+
+    i = 0
+    while i < length
+      yield(self[i], i)
+      i += 1
+    end
+    self
+  end
 end
 
-[11, 79, 15, 43, 38, 1].my_each { |items| items * 2 }
+[11, 79, 15, 43, 38, 1].my_each { |items| puts items * 2 }
+
+[27, 76, 48, 37, 24, 12, 47].my_each_with_index { |_x, y| puts y }
