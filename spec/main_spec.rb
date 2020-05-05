384d6772d3d62888f8e95ce0a63392aafffe0856
Rossiel Carranza <rossielcarranza@gmail.com>
2020-05-05 17:34:41 -0300

    Add specs for two methods
    - my_each
    - my_each_with_index

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,2 +1,25 @@
-require 'main'
+require './main'
 
+describe Enumerable do
+  let(:array) { [1, 2, 3, 4, 5, 6] }
+
+  describe '#my_each' do
+    it 'returns an enumerator if no block is given' do
+      expect(array.my_each).to be_an_instance_of(Enumerator)
+    end
+
+    it 'returns the element if a block is given' do
+      expect(array.my_each { |x| x * 2 }).to eql(array)
+    end
+  end
+
+  describe '#my_each_with_index' do
+    it 'returns an enumerator if no block is given' do
+      expect(array.my_each_with_index).to be_an_instance_of(Enumerator)
+    end
+
+    it 'returns the element if a block is given' do
+      expect(array.my_each_with_index { |x, _| x * 2 }).to eql(array)
+    end
+  end
+end
